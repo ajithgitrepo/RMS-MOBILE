ae8752d0a9c2ca3530c041427abe8ad7d74af32e
banuvijai <91789766+vinoth32-1@users.noreply.github.com>
2022-02-03 10:14:51 +0530

    morn push

--- lib/api/FMapi/cdereportapi.dart
+++ lib/api/FMapi/cdereportapi.dart
@@ -5,12 +5,13 @@ import 'package:merchandising/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:merchandising/Constants.dart';
 import 'package:merchandising/offlinedata/sharedprefsdta.dart';
-
 String empreportdata;
-Future CDEReportingDetails() async {
-  Map DBrequestData = {'emp_id': '${DBrequestdata.receivedempid}'};
-  http.Response EmpReport = await http.post(
-    CDEReportingDet,
+Future CDEReportingDetails() async{
+
+  Map DBrequestData = {
+    'emp_id': '${DBrequestdata.receivedempid}'
+  };
+  http.Response EmpReport = await http.post(CDEReportingDet,
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
@@ -21,69 +22,110 @@ Future CDEReportingDetails() async {
   if (EmpReport.statusCode == 200) {
     print('CDE reporting details done');
     CDEreportdata.cdeempid = [];
-    CDEreportdata.mername = [];
-    CDEreportdata.mersname = [];
-    CDEreportdata.cdeempname = [];
-    CDEreportdata.cdeempsname = [];
     CDEreportdata.startdate = [];
     CDEreportdata.merchandisersid = [];
     CDEreportdata.enddate = [];
+    CDEreportdata.merch_names = [];
     print('reporting details');
     empreportdata = EmpReport.body;
     addempdetailesreport(empreportdata);
     var decodeddata = jsonDecode(empreportdata);
-
     for (int u = 0; u < decodeddata['data'].length; u++) {
       CDEreportdata.cdeempid.add(decodeddata["data"][u]['cde_id']);
-
-      CDEreportdata.mername
-          .add(decodeddata["data"][u]['merchandiser']['first_name']);
-      if (decodeddata["data"][u]['merchandiser']['surname'] != "") {
-        CDEreportdata.mersname
-            .add(decodeddata["data"][u]['merchandiser']['surname']);
-      } else {
-        CDEreportdata.mersname.add("");
-      }
-
-      CDEreportdata.cdeempname
-          .add(decodeddata["data"][u]['cde_reporting']['first_name']);
-      if (decodeddata["data"][u]['cde_reporting']['surname'] != "") {
-        CDEreportdata.cdeempsname
-            .add(decodeddata["data"][u]['cde_reporting']['surname']);
-      } else {
-        CDEreportdata.cdeempsname.add("");
-      }
-
       CDEreportdata.startdate.add(decodeddata["data"][u]['reporting_date']);
       CDEreportdata.enddate.add(decodeddata["data"][u]['reporting_end_date']);
-      CDEreportdata.merchandisersid
-          .add(decodeddata["data"][u]['merchandiser_id']);
+      CDEreportdata.merchandisersid.add(decodeddata["data"][u]['merchandiser_id']);
+      // CDEreportdata.merch_names.add(decodeddata["data"][u]['merchandiser']);
 
-      // print(decodeddata["data"][u]['cde_reporting']['first_name']);
+      if(decodeddata["data"][u]['merchandiser'] == null) {
+        //  print(' ' );
+        CDEreportdata.merch_names.add(' ');
+
+      }
+      else
+      {
+
+        var familyMembers = decodeddata["data"][u]["merchandiser"];
+        print(familyMembers['first_name']);
+
+        // try {
+        //
+        //
+        //   CDEreportdata.merch_names.add(familyMembers['first_name']);
+        //
+        // } on Exception catch (exception) {
+        //
+        //   print('error1');
+        //
+        // } catch (error) {
+        //   print('error2');
+        // }
+
+
+        // decodeddata['data'].forEach((item) {
+        // mapping your family_members array
+        // decodeddata["data"][u]['merchandiser'].forEach((nestedItem) {
+        //   if(nestedItem['merchandiser'] != null)
+        //      print('tee');
+        // });
+        // });
+
+
+      }
+
+      //  Unhandled Exception: type '(dynamic) => Null'
+      // var recordsList = decodeddata["data"];
+      // for (var record in recordsList) {
+      //   var familyMembers = record["merchandiser"];
+      //   //print(familyMembers);
+      //   for (var familyMember in familyMembers){ //prints the name of each family member
+      //     var familyMemberName = familyMember;
+      //     print('eeeee');
+      //   }
+      // }
+      // for (var familyMember in familyMembers){ //prints the name of each family member
+      //   var familyMemberName = familyMember["name"];
+      //   print(familyMemberName);
+      // }
+
+      //   for (var familyMember in familyMembers) { //prints the name of each family member
+      //     var familyMemberName = familyMember["employee_id"];
+      //
+      //     print(familyMemberName);
+      //   }
+      //   }
+
+      // mapping your records array
+      // decodeddata['data'].forEach((item){
+      //   // mapping your family_members array
+      //   decodeddata['merchandiser'].forEach((newitem){
+      //
+      //      print(newitem['first_name']);
+      //
+      //   });
+      // });
 
     }
-
     // print(CDEreportdata.merchandisersid.length);
-    // print(CDEreportdata.cdeempsname);
-    // print(CDEreportdata.cdeempid.length);
+    // print(CDEreportdata.merchandisersid);
+    print(CDEreportdata.merch_names);
 
   }
   if (EmpReport.statusCode != 200) {
     print(EmpReport.body);
   }
+
 }
 
 class CDEreportdata {
   static List<dynamic> cdeempid = [];
-
-  static List<dynamic> mername = [];
-  static List<dynamic> mersname = [];
-  static List<dynamic> cdeempname = [];
-  static List<dynamic> cdeempsname = [];
-
-  static List<dynamic> startdate = [];
+  static List<dynamic> startdate=[];
   static List<dynamic> merchandisers = [];
-  static List<dynamic> enddate = [];
-  static List<dynamic> merchandisersid = [];
-  static List<dynamic> fmnames = [];
+  static List<dynamic> enddate=[];
+  static List<dynamic> merchandisersid=[];
+  static List<dynamic> fmnames=[];
+  static List<dynamic> merch_names=[];
+
+
+
 }
